94573704ed0232695dd939f5d3a0c7cb7cfe5bc1
Igor Michalak <git@igormichalak.com>
2025-02-27 23:53:36 +0100

    WAIIG page 187

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -72,9 +72,65 @@ func Eval(env *object.Environment, node ast.Node) object.Object {
 			return right
 		}
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Env: env, Body: body}
+	case *ast.CallExpression:
+		function := Eval(env, node.Function)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(env, node.Arguments)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
+		return applyFunction(function, args)
 	}
 
 	return nil
+}
+
+func evalExpressions(env *object.Environment, exps []ast.Expression) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(env, e)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+
+	return result
+}
+
+func applyFunction(fn object.Object, args []object.Object) object.Object {
+	function, ok := fn.(*object.Function)
+	if !ok {
+		return newError("not a function: %s", fn.Type())
+	}
+
+	extendedEnv := extendFunctionEnv(function, args)
+	evaluated := Eval(extendedEnv, function.Body)
+	return unwrapReturnValue(evaluated)
+}
+
+func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
+	env := object.NewEnclosedEnvironment(fn.Env)
+
+	for paramIdx, param := range fn.Parameters {
+		env.Set(param.Value, args[paramIdx])
+	}
+
+	return env
+}
+
+func unwrapReturnValue(obj object.Object) object.Object {
+	if returnValue, ok := obj.(*object.ReturnValue); ok {
+		return returnValue.Value
+	}
+	return obj
 }
 
 func evalIdentifier(env *object.Environment, node *ast.Identifier) object.Object {
