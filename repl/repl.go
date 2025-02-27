6170089b4880584ac67bda852472fa39bf1a4685
Igor Michalak <git@igormichalak.com>
2025-02-27 17:32:07 +0100

    WAIIG page 147

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/igormichalak/monkey/evaluator"
 	"github.com/igormichalak/monkey/lexer"
 	"github.com/igormichalak/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
