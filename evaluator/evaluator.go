e001a39683c9bc489a4b9a82a6625398b970bd6b
Igor Michalak <git@igormichalak.com>
2025-02-27 20:07:21 +0100

    WAIIG page 161

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,9 +17,12 @@ func Eval(node ast.Node) object.Object {
 	// Statements
 	case *ast.Program:
 		return evalStatements(node.Statements)
-
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 
 	// Expressions
 	case *ast.IntegerLiteral:
@@ -36,6 +39,31 @@ func Eval(node ast.Node) object.Object {
 	}
 
 	return nil
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
