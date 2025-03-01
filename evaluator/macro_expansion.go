755b125207c189aaa7ae8c70154001f52173fdfe
Igor Michalak <git@igormichalak.com>
2025-03-01 17:15:50 +0100

    WAIIG lost chapter: macro definitions

---
+++ evaluator/macro_expansion.go
@@ -0,0 +1,52 @@
+package evaluator
+
+import (
+	"github.com/igormichalak/monkey/ast"
+	"github.com/igormichalak/monkey/object"
+)
+
+func DefineMacros(env *object.Environment, program *ast.Program) {
+	definitions := []int{}
+
+	for i, statement := range program.Statements {
+		if isMacroDefinition(statement) {
+			addMacro(env, statement)
+			definitions = append(definitions, i)
+		}
+	}
+
+	for i := len(definitions) - 1; i >= 0; i = i - 1 {
+		definitionIndex := definitions[i]
+		program.Statements = append(
+			program.Statements[:definitionIndex],
+			program.Statements[definitionIndex+1:]...,
+		)
+	}
+}
+
+func isMacroDefinition(node ast.Statement) bool {
+	letStatement, ok := node.(*ast.LetStatement)
+	if !ok {
+		return false
+	}
+
+	_, ok = letStatement.Value.(*ast.MacroLiteral)
+	if !ok {
+		return false
+	}
+
+	return true
+}
+
+func addMacro(env *object.Environment, stmt ast.Statement) {
+	letStatement, _ := stmt.(*ast.LetStatement)
+	macroLiteral, _ := letStatement.Value.(*ast.MacroLiteral)
+
+	macro := &object.Macro{
+		Parameters: macroLiteral.Parameters,
+		Env:        env,
+		Body:       macroLiteral.Body,
+	}
+
+	env.Set(letStatement.Name.Value, macro)
+}
